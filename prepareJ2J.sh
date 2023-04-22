88de791737b23cf7c310cbc5c12f6162396176c6
NMS-LemonTec <92583670+NMS-LemonTec@users.noreply.github.com>
2023-04-22 20:29:08 +0800

    Create prepareJ2J.sh
    add the runtime

---
+++ prepareJ2J.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# by https://github.com/spiritLHLS/lxc
+
+
+
+#root权限
+root_need(){
+    if [[ $EUID -ne 0 ]]; then
+        echo -e "${Red}Error:This script must be run as root!${Font}"
+        exit 1
+    fi
+}
+
+#检测ovz
+ovz_no(){
+    if [[ -d "/proc/vz" ]]; then
+        echo -e "${Red}Your VPS is based on OpenVZ，not supported!${Font}"
+        exit 1
+    fi
+}
+apt update
+apt install curl wget sudo dos2unix ufw snapd -y
+snap install lxd
+/snap/bin/lxd init
+! lxc -h >/dev/null 2>&1 && echo 'alias lxc="/snap/bin/lxc"' >> /root/.bashrc && source /root/.bashrc
+export PATH=$PATH:/snap/bin
+main
